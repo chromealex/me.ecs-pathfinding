d4ebf5871fd73a997f01433fdca479d5a348fe8a
Alex Silaev <chrome.alex@gmail.com>
2022-08-05 12:52:22 +0300

    Initial Commit

---
+++ ME.ECS.Essentials.Pathfinding/Features/PathfindingStructComponentsGenerator.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ME.ECS {
+
+    public static class PathfindingComponentsInitializer {
+    
+        public static void Init(ref ME.ECS.StructComponentsContainer structComponentsContainer) {
+    
+        }
+
+        public static void InitEntity(Entity entity) {
+        
+        }
+
+    }
+
+}
